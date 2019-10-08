bded04a18228d747b3699c1b2bac4d1f98848f7d
Roberto Pérez <43781972+Robertostrikers@users.noreply.github.com>
2019-10-08 20:24:15 +0200

    Added base game
    I added al te game with the first part

---
+++ SCP - Text Adventure (Español)/Assets/Scrpts/AdventureGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*
+ * Este codigo es la fuente principal, 
+ */
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //llama a un metodo de state.cs para recoger el texto de startingState
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStage = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStage[0];
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStage[1];
+        }else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStage[2];
+        }
+        textComponent.text = state.GetStateStory(); //El campo texto de objeto texto es igual al campo texto de story text
+    }
+}
---
+++ SCP - Text Adventure (Español)/Assets/Scrpts/State.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Este codigo permite crear un gameobject "state".
+ * El object "state" guarda informacion string (nombrado storyText)
+ * */
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // Start is called before the first frame update
+    [TextArea(30, 30)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
+
+
+
